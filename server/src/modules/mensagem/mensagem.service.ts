4613c883f0b82a104b1c413998bd78050abf9762
Giovanny Lucas Alves de Oliveira <giovannylucax@gmail.com>
2020-12-12 11:44:43 -0300

    adicionado campo com dados dos anexos

--- server/src/modules/mensagem/mensagem.service.ts
+++ server/src/modules/mensagem/mensagem.service.ts
@@ -113,39 +113,34 @@ export class MensagemService {
     const offset = pagination.page ? pagination.page < 0 ? 0 : pagination.page - 1 : 0
     const limit = pagination.limit ? pagination.limit < 0 ? 50 : pagination.limit : 50
 
-    const caseAnexoSelect = `
-      (SELECT COUNT(*) FROM anexo WHERE anexo.id_mensagem = mensagem.id)::integer anexos
-    `
-    const caseRemetenteSelect = `
+    return await this.repo.query(`
+    SELECT
+      mensagem.id,
+      mensagem.id_conversa,
+      mensagem.id_remetente,
       CASE
         WHEN mensagem.id_remetente = '${id_usuario}' THEN true
         ELSE false
-      END AS e_remetente
-    `
-
-    const a = await this.repo.createQueryBuilder()
-      .distinct()
-      .select([
-        'mensagem.id id',
-        'mensagem.id_conversa id_conversa',
-        'mensagem.id_remetente id_remetente',
-        caseRemetenteSelect,
-        caseAnexoSelect,
-        'mensagem.texto texto',
-        'mensagem.data_leitura data_leitura',
-        'mensagem.data_envio data_envio',
-        'array_agg((anexo.sequencia, anexo.instrucao)) anexos'
-      ])
-      .from(Mensagem, 'mensagem')
-      .leftJoin(Anexo, 'anexo', 'anexo.id_mensagem = mensagem.id')
-      .where('mensagem.id_conversa = :id_conversa', { id_conversa })
-      .orderBy('mensagem.data_envio', 'ASC')
-      .groupBy('mensagem.id_conversa, mensagem.id')
-      .limit(limit)
-      .offset(limit * offset)
-      .getRawMany()
-
-    console.log(a);
-    return a
+      END e_remetente,
+      mensagem.texto,
+      mensagem.data_leitura,
+      mensagem.data_envio,
+      COUNT(anexo.sequencia)::integer anexos,
+      CASE
+        WHEN (COUNT(anexo.*) > 0)
+        THEN
+          json_agg(
+            (SELECT row_to_json(_) FROM (SELECT anexo.sequencia, anexo.instrucao) as _)
+          )
+        ELSE '[]'::json
+      END dados_anexos
+    FROM mensagem
+    LEFT JOIN anexo ON anexo.id_mensagem = mensagem.id
+    WHERE mensagem.id_conversa = $1
+    GROUP BY mensagem.id_conversa, mensagem.id
+    ORDER BY mensagem.data_envio ASC
+    LIMIT $2
+    OFFSET $3
+    `, [ id_conversa, limit, limit * offset ])
   }
 }
